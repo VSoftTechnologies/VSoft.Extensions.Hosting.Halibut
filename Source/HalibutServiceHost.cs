9178e0464d58467454c15543b961eb888bbe34d9
Vincent Parrett <vincent@finalbuilder.com>
2019-08-13 09:27:23 +1000

    Fixed issue with trust not working correctly.

--- Source/HalibutServiceHost.cs
+++ Source/HalibutServiceHost.cs
@@ -8,59 +8,55 @@ using System.Threading.Tasks;
 
 namespace VSoft.Extensions.Hosting.Halibut
 {
-    public class HalibutServiceHost : IHostedService
-    {
-        private readonly HalibutHostOptions _options;
-        private readonly IServiceFactory _serviceFactory;
-        private readonly ITrustProvider _trustProvider;
-        private HalibutRuntime _runtime;
+	public class HalibutServiceHost : IHostedService
+	{
+		private readonly HalibutHostOptions _options;
+		private readonly IServiceFactory _serviceFactory;
+		private readonly ITrustProvider _trustProvider;
+		private HalibutRuntime _runtime;
 
-        public HalibutServiceHost(HalibutHostOptions options, IServiceFactory serviceFactory, ITrustProvider trustProvider)
-        {
-            _options = options;
-            _serviceFactory = serviceFactory;
-            _trustProvider = trustProvider;
-        }
+		public HalibutServiceHost(HalibutHostOptions options, IServiceFactory serviceFactory, ITrustProvider trustProvider)
+		{
+			_options = options;
+			_serviceFactory = serviceFactory;
+			_trustProvider = trustProvider;
+		}
 
-        public Task StartAsync(CancellationToken cancellationToken)
-        {
+		public Task StartAsync(CancellationToken cancellationToken)
+		{
 
-            var certificate = new X509Certificate2(_options.CertificateFile);
-            _runtime = new HalibutRuntime(_serviceFactory, certificate);
+			var certificate = new X509Certificate2(_options.CertificateFile);
+			_runtime = new HalibutRuntime(_serviceFactory, certificate, _trustProvider);
 
-            if (_options.Trust.Count > 0)
-            {
-                foreach (var item in _options.Trust)
-                {
-                    _trustProvider.Add(item);
-                }
-            }
+			if (_options.Trust.Count > 0)
+				foreach (var item in _options.Trust)
+					_trustProvider.Add(item);
 
-            _runtime.OnUnauthorizedClientConnect = _options.OnUnauthorizedClientConnect;
+			_runtime.OnUnauthorizedClientConnect = _options.OnUnauthorizedClientConnect;
 
-            switch (_options.Mode)
-            {
-                case HalibutServerMode.Listening:
-                    _runtime.Listen(_options.Listen.EndPoint);
-                    break;
-                case HalibutServerMode.Polling:
-                    _runtime.Poll(new Uri($"poll://{_options.Polling.Subscription}"), new ServiceEndPoint(new Uri(_options.Polling.BaseUri), _options.Polling.RemoteThumbPrint, _options.ProxyDetails));
+			switch (_options.Mode)
+			{
+				case HalibutServerMode.Listening:
+					_runtime.Listen(_options.Listen.EndPoint);
+					break;
+				case HalibutServerMode.Polling:
+					_runtime.Poll(new Uri($"poll://{_options.Polling.Subscription}"), new ServiceEndPoint(new Uri(_options.Polling.BaseUri), _options.Polling.RemoteThumbPrint, _options.ProxyDetails));
 
-                    break;
-                case HalibutServerMode.WebSocketPolling:
-                    _runtime.Poll(new Uri($"poll://{_options.Polling.Subscription}"), new ServiceEndPoint(new Uri(_options.Polling.BaseUri), _options.Polling.RemoteThumbPrint,_options.ProxyDetails));
-                    break;
-            }
+					break;
+				case HalibutServerMode.WebSocketPolling:
+					_runtime.Poll(new Uri($"poll://{_options.Polling.Subscription}"), new ServiceEndPoint(new Uri(_options.Polling.BaseUri), _options.Polling.RemoteThumbPrint, _options.ProxyDetails));
+					break;
+			}
 
-            return Task.CompletedTask;
-        }
+			return Task.CompletedTask;
+		}
 
-        public Task StopAsync(CancellationToken cancellationToken)
-        {
-            _runtime.Dispose();
-            _runtime = null;
+		public Task StopAsync(CancellationToken cancellationToken)
+		{
+			_runtime.Dispose();
+			_runtime = null;
 
-            return Task.CompletedTask;
-        }
-    }
+			return Task.CompletedTask;
+		}
+	}
 }
